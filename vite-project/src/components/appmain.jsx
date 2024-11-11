776ab56efbeb41a680eb128216d869b101ad1239
alicemariaarrica <alicemariaarrica@gmail.com>
2024-11-11 22:38:46 +0100

    appmain-welcome-task-commit

--- vite-project/src/components/appmain.jsx
+++ vite-project/src/components/appmain.jsx
@@ -1,6 +1,7 @@
 export default function AppMain() {
-
     return (
-        <main>this is the body</main>
-    )
+        <div className="title">
+            <h1>Welcome to my React app</h1>
+        </div>
+    );
 }
